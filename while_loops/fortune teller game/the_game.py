d34a6cafcfe8226a2626d7148713e767dddc6fcb
Daria Vucea <daria@vav.ro>
2019-05-13 21:25:00 -0400

    Finish the game fam

--- while_loops/fortune teller game/the_game.py
+++ while_loops/fortune teller game/the_game.py
@@ -1,4 +1,16 @@
 import fortunes_list
+import random
 
-print(fortunes_list.the_list[4])
+#TODO 1: validation for user input other than Y or N
+#TODO 2: fix program to not show line 8 twice 
 
+exit_game = False
+print('Welcome to Fortune Teller Simulator.')
+print(fortunes_list.the_list[random.randint(0, len(fortunes_list.the_list)-1)])
+
+while exit_game == False:
+    print(fortunes_list.the_list[random.randint(0, len(fortunes_list.the_list)-1)])
+    user_input = input('Do you wish for another fortune? (Y/N)\n')
+    if user_input == 'N':
+        exit_game = True
+        print('bYe oLd fRiEnD!')
