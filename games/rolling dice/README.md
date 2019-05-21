af950d99a462a6a66286dd6a3b2b48f191e19092
Christiana Vucea <christiana@vav.ro>
2019-05-20 21:04:35 -0400

    Finished Endgame

--- games/rolling dice/README.md
+++ games/rolling dice/README.md
@@ -7,3 +7,10 @@ Rolling Dice Simulation
 - These numbers will also be between 1-6 and be added 
 - The winner is whoever got the bigger sum.
 2) Same as 1) but we add a "do you want to play again?"
+3) Endgame:
+- makes user type in the sum they want to reach for
+- for each turn we display users dices and the sum
+- after each turn, we ask user to press enter
+- whichever user reaches the sum first wins
+- If both reach the sum on the same round, whichever has the largest score wins. 
+- Print messeges on the screen accordingly
---
+++ games/rolling dice/rolling_dice_end_game.py
@@ -0,0 +1,31 @@
+import random 
+
+game_end = False
+user_sum = 0
+computer_sum = 0
+
+print('Welcome to the rolling dice game!')
+target = int(input('Please type in a number to set your target:\n'))
+input('Press enter to continue')
+
+while game_end == False:
+    user_dice_1 = random.randint(1,6)
+    user_dice_2 = random.randint(1,6)
+    computer_dice_1 = random.randint(1,6)
+    computer_dice_2 = random.randint(1,6)
+    user_sum = user_dice_1 + user_dice_2 + user_sum
+    computer_sum = computer_dice_1 + computer_dice_2 + computer_sum
+    print('Your dice numbers are {} and {}! Your total is {}.'.format(user_dice_1, user_dice_2, user_sum))
+    print('The computer dice numbers are {} and {} The computer total is {}!'.format(computer_dice_1, computer_dice_2, computer_sum))
+    
+    if user_sum > target or computer_sum > target:
+        game_end = True
+    else:
+        input('Press enter to continue\n')
+else:
+    if user_sum > computer_sum:
+        print('You won!')
+    elif user_sum < computer_sum:
+        print('Computer won, you madlad!')
+    else:
+        print('There has been a tie!')
