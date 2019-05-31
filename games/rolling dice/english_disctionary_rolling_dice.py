563ec97b273716ac12a8256a394e8ac257661f5f
Daria Vucea <daria@vav.ro>
2019-05-30 21:46:08 -0400

    English dictionary for rolling dice game

---
+++ games/rolling dice/english_disctionary_rolling_dice.py
@@ -0,0 +1,11 @@
+
+rolling_dice_english = {
+    'target' : 'Please type in a number to set your target:\n',
+    'error' : 'That is not an integer.',
+    'continue' : 'Press enter to continue',
+    'person dice' : 'Your dice numbers are {} and {}! Your total is {}.',
+    'computer dice' : 'The computer dice numbers are {} and {} The computer total is {}!',
+    'win' : 'You won!',
+    'loose' : 'Computer won, you madlad!',
+    'tie' : 'There has been a tie!'
+}
