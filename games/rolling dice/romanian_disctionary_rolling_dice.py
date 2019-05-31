2408dac6903eddde529c019b28dfb6d098c54d1b
Christiana Vucea <christiana@vav.ro>
2019-05-31 17:27:06 -0400

    My part of the homework

---
+++ games/rolling dice/romanian_disctionary_rolling_dice.py
@@ -0,0 +1,11 @@
+
+rolling_dice_romanian = {
+    'target' : 'Introduceți un număr care să fie setat ca țintă:\n',
+    'error' : 'Tinta aleasa nu este un număr întreg.',
+    'continue' : 'Apăsați enter pentru a continua',
+    'person dice' : 'Numerele de zaruri sunt {} și {}! Totalul este {}.',
+    'computer dice' : 'Numerele de cifre ale calculatoarelor sunt {} și {} Totalul calculatorului este {}!',
+    'win' : 'Ai castigat!',
+    'loose' : 'Calculatorul a câștigat!',
+    'tie' : 'Egalitate!!'
+}
