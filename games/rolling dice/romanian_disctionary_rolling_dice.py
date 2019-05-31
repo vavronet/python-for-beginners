742a267b717231d21d97cd027b32681ac4f78c43
Valentin Vucea <valentin@itworksdc.com>
2019-05-31 19:39:12 -0400

    REwording values for romanian disctionary

--- games/rolling dice/romanian_disctionary_rolling_dice.py
+++ games/rolling dice/romanian_disctionary_rolling_dice.py
@@ -3,8 +3,8 @@ rolling_dice_romanian = {
     'target' : 'Introduceți un număr care să fie setat ca țintă:\n',
     'error' : 'Tinta aleasa nu este un număr întreg.',
     'continue' : 'Apăsați enter pentru a continua',
-    'person dice' : 'Numerele de zaruri sunt {} și {}! Totalul este {}.',
-    'computer dice' : 'Numerele de cifre ale calculatoarelor sunt {} și {} Totalul calculatorului este {}!',
+    'person dice' : 'Zarurile Dvs. sunt {} și {}! Totalul este {}.',
+    'computer dice' : 'Zarurile calculatorului sunt {} și {} Totalul calculatorului este {}!',
     'win' : 'Ai castigat!',
     'loose' : 'Calculatorul a câștigat!',
     'tie' : 'Egalitate!!'
