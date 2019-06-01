bffe04448ffa60005a26352324fc1a7e2d016f3e
Christiana Vucea <christiana@vav.ro>
2019-05-31 20:17:00 -0400

    Finished multi-language rolling dice game.

--- games/rolling dice/rolling_dice_end_game_multi_language.py
+++ games/rolling dice/rolling_dice_end_game_multi_language.py
@@ -1,4 +1,6 @@
 import random 
+import english_disctionary_rolling_dice
+import romanian_disctionary_rolling_dice
 
 game_end = False
 user_sum = 0
@@ -6,18 +8,8 @@ computer_sum = 0
 
 print('Welcome to the rolling dice game!')
 
-rolling_dice_english = {
-    'input_target': 'Please type in a number to set your target:\n',
-    'integer_error': 'That is not an integer.'
-}
-
-rolling_dice_romanian = {
-    'input_target': 'Te rog sa alegi un numar pentru scorul final:\n',
-    'integer_error': 'Nu ai introdus un numar.'
-}
-
 language_is_selected = False
-language_dictionary = rolling_dice_english
+language_dictionary = english_disctionary_rolling_dice.rolling_dice_english
 
 while language_is_selected == False:
     language = input('Please select the language: e/English, r/Romanian \n')
@@ -26,7 +18,7 @@ while language_is_selected == False:
         print('You selected English.')
     elif language == 'r':
         language_is_selected = True
-        language_dictionary = rolling_dice_romanian
+        language_dictionary = romanian_disctionary_rolling_dice.rolling_dice_romanian
         print('Ai selectat limba Romana.')
     else:
         print('Invalid input.')
@@ -40,7 +32,7 @@ while target_is_integer == False:
     except ValueError:
         print(language_dictionary['integer_error'])
 else: 
-    input('Press enter to continue')
+    input(language_dictionary['continue'])
 
 while game_end == False:
     user_dice_1 = random.randint(1,6)
@@ -49,17 +41,17 @@ while game_end == False:
     computer_dice_2 = random.randint(1,6)
     user_sum = user_dice_1 + user_dice_2 + user_sum
     computer_sum = computer_dice_1 + computer_dice_2 + computer_sum
-    print('Your dice numbers are {} and {}! Your total is {}.'.format(user_dice_1, user_dice_2, user_sum))
-    print('The computer dice numbers are {} and {} The computer total is {}!'.format(computer_dice_1, computer_dice_2, computer_sum))
+    print(language_dictionary['person_dice'].format(user_dice_1, user_dice_2, user_sum))
+    print(language_dictionary['computer_dice'].format(computer_dice_1, computer_dice_2, computer_sum))
     
     if user_sum > target or computer_sum > target:
         game_end = True
     else:
-        input('Press enter to continue\n')
+        input(language_dictionary['continue'])
 else:
     if user_sum > computer_sum:
-        print('You won!')
+        print(language_dictionary['win'])
     elif user_sum < computer_sum:
-        print('Computer won, you madlad!')
+        print(language_dictionary['loose'])
     else:
-        print('There has been a tie!')
+        print(language_dictionary['tie'])
