75d3de3fae575d009daa1403f5da143af2f9af7e
Daria Vucea <daria@vav.ro>
2019-06-03 19:54:34 -0400

    Count letters  - wip

---
+++ dictionaries/count_letters.py
@@ -0,0 +1,12 @@
+# Retrieve the letters from a given text and store them in a dictionary, counting how many times they occured.
+# Don't count commas, dots, spaces, aphostrophes.
+# Make it so that capital letters are included.
+
+source = 'After some minutes a sharp hissing reached my ears. I felt a distinct sensation of cold rising from my feet to my chest. Apparently a stopcock inside the boat was letting in water from outside, which overran us and soon filled up the room. Contrived in the Nautilus’s side, a second door then opened. We were lit by a subdued light. An instant later our feet were treading the bottom of the sea.'
+
+# counting = {
+#    'a': 10,
+#    'b': 5,
+#    'c': 6
+# }
+
