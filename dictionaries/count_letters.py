31406085cea453986a54c32489c4906f1004fcc5
Christiana Vucea <christiana@vav.ro>
2019-06-03 20:34:14 -0400

    Finished counting letters

--- dictionaries/count_letters.py
+++ dictionaries/count_letters.py
@@ -4,9 +4,24 @@
 
 source = 'After some minutes a sharp hissing reached my ears. I felt a distinct sensation of cold rising from my feet to my chest. Apparently a stopcock inside the boat was letting in water from outside, which overran us and soon filled up the room. Contrived in the Nautilus’s side, a second door then opened. We were lit by a subdued light. An instant later our feet were treading the bottom of the sea.'
 
-# counting = {
-#    'a': 10,
-#    'b': 5,
-#    'c': 6
-# }
+# remove periods, commas, spaces, and aphostrophes.
+source = source.replace('.','')
+source = source.replace(',','')
+source = source.replace('’','')
+source = source.replace(' ','')
 
+# convert to smallcaps
+source = source.lower()
+
+# initialize dictionary
+counting = {}
+
+# loop through the source 
+for x in source:
+    # check if x exists in dictionary already 
+    if x in counting.keys():
+        counting[x] = counting[x] + 1
+    else:
+        counting[x] = 1
+
+print(sorted(counting))
