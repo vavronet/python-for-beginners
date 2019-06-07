88e4313c61953b3978a7d4dd45c0f6d2cbeabc32
Valentin Vucea <valentin@itworksdc.com>
2019-06-07 19:55:22 -0400

    First function examples

---
+++ functions/example_1.py
@@ -0,0 +1,18 @@
+def do_something():
+    print('I\'m doing something!')
+
+def do_something_else(message):
+    print(message)
+
+def cilinder_volume(r, h):
+    pi = 3.14159
+    volume = pi * r * r * h
+    return volume
+
+# do_something()
+# do_something_else('Hello Vali')
+# cilinder_volume(3, 5)
+
+result = cilinder_volume(3,5)
+
+print(result)
