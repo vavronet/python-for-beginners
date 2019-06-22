ee63aa312f840d0f250d862480d7c082acbacbb8
Christiana Vucea <christiana@vav.ro>
2019-06-21 20:28:45 -0400

    Show placeholder function

---
+++ functions/examples/example_9.py
@@ -0,0 +1,25 @@
+# Create a function that retrieves a random element from a list of strings and displays on the screen an underscore character for each letter.
+import random
+
+def get_random_element(a_list):
+    random_index = random.randint(0,len(a_list)-1)
+    random_element = a_list[random_index]
+    return random_element
+
+def show_placeholders(word, letter):
+    word_capitals = word.upper()
+    letter_capital = letter.upper()
+    placeholders = ''
+    for x in word_capitals:
+        if x == letter_capital:
+            placeholders = placeholders + x + ' '
+        else:
+            placeholders = placeholders + '_ '
+    return placeholders
+
+b_list = ['Lorem', 'ipsum', 'dolor', 'amet', 'consectetur', 'adipiscing', 'elit', 'eiusmod', 'tempor', 'incididunt', 'labore', 'et', 'dolore', 'magna', 'aliqua']
+
+the_word = get_random_element(b_list)
+
+print(the_word)
+print(show_placeholders(the_word, 'm'))
