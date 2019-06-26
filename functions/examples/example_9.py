04e4f0ed98d192e08ec154e05725de8640826896
Daria Vucea <daria@vav.ro>
2019-06-25 20:51:40 -0400

    Added guessed letters

--- functions/examples/example_9.py
+++ functions/examples/example_9.py
@@ -6,13 +6,16 @@ def get_random_element(a_list):
     random_element = a_list[random_index]
     return random_element
 
-def show_placeholders(word, letter):
+def show_placeholders(word, letter, guessed_letters):
     word_capitals = word.upper()
     letter_capital = letter.upper()
     placeholders = ''
     for x in word_capitals:
         if x == letter_capital:
             placeholders = placeholders + x + ' '
+            guessed_letters.append(letter_capital)
+        elif (x in guessed_letters) == True:
+             placeholders = placeholders + x + ' '
         else:
             placeholders = placeholders + '_ '
     return placeholders
@@ -20,6 +23,9 @@ def show_placeholders(word, letter):
 b_list = ['Lorem', 'ipsum', 'dolor', 'amet', 'consectetur', 'adipiscing', 'elit', 'eiusmod', 'tempor', 'incididunt', 'labore', 'et', 'dolore', 'magna', 'aliqua']
 
 the_word = get_random_element(b_list)
+guessed = ['A', 'I']
 
+print(guessed)
 print(the_word)
-print(show_placeholders(the_word, 'm'))
+print(show_placeholders(the_word, 'o', guessed))
+print(guessed)
