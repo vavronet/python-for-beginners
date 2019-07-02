20ef1a8374a8b5e315652b5cf676cd3370b96404
Christiana Vucea <christiana@vav.ro>
2019-07-01 20:02:41 -0400

    Created the skeleton of the Hangman game

--- functions/examples/example_9.py
+++ functions/examples/example_9.py
@@ -1,11 +1,13 @@
 # Create a function that retrieves a random element from a list of strings and displays on the screen an underscore character for each letter.
 import random
 
+# Returns a random element from a given list 
 def get_random_element(a_list):
     random_index = random.randint(0,len(a_list)-1)
     random_element = a_list[random_index]
     return random_element
 
+# Returns the formatted word with underscores for the unguessed letters 
 def show_placeholders(word, letter, guessed_letters):
     word_capitals = word.upper()
     letter_capital = letter.upper()
---
+++ games/hangman/README.md
@@ -0,0 +1,13 @@
+Hangman game
+
+Rules: 
+- Number of tries: 8
+- Only one word per round
+- Display a warning if the word was already guessed 
+- Display a warning if the word wasn't already guessed
+ 
+Requirements (v1):
+- Welcome message stating the rules
+- The list of available words will be stored in another file
+- For each round, the number of tries left will be displayed
+- Game over message
